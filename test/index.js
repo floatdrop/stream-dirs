806c70d9dcca8c7a9035d3bd4f412457e0201750
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-18 00:31:21 +0600

    test(dirs): emit error on fs.stat failure

--- test/index.js
+++ test/index.js
@@ -2,6 +2,15 @@
 
 var assert = require('stream-assert');
 var readdir = require('../index.js');
+var should = require('should');
+
+it('should emit error from fs.stat', function (done) {
+    readdir('undefined')
+        .on('error', function (err) {
+            should.exist(err);
+            done();
+        });
+});
 
 it('should work on empty dir', function (done) {
     readdir(__dirname + '/fixtures/empty').pipe(assert.length(0)).on('end', done);
