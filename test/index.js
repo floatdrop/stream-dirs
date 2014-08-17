46c516145f4572ae8e4ba07f388c6fcaa6740b8a
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-18 00:43:16 +0600

    test(dirs): add test on fs.stat exception

--- test/index.js
+++ test/index.js
@@ -3,14 +3,30 @@
 var assert = require('stream-assert');
 var readdir = require('../index.js');
 var should = require('should');
+var sinon = require('sinon');
+var fs = require('fs');
 
-it('should emit error from fs.stat', function (done) {
+it('should emit error from fs.readdir', function (done) {
     readdir('undefined')
         .on('error', function (err) {
             should.exist(err);
             done();
         });
 });
+
+it('should emit error from fs.stat', function (done) {
+    sinon.stub(fs, 'stat', function (path, cb) {
+        cb(new Error('Bang!'));
+    });
+
+    readdir(__dirname + '/fixtures')
+        .on('error', function (err) {
+            fs.stat.restore();
+            should.exist(err);
+            done();
+        });
+});
+
 
 it('should work on empty dir', function (done) {
     readdir(__dirname + '/fixtures/empty').pipe(assert.length(0)).on('end', done);
